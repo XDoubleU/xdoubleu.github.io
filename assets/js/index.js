59c20b3a96926a0da34905b46a61594bbe7fe1e5
Xander Warszawski <xander@xdoubleu.com>
2023-02-02 12:57:45 +0100

    V3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,60 +1,8 @@
 const COUNT_MAX = 8;
 
-let count = 0;
-
-
-function getExtraRepos(repos){
-    $.each(repos, function(index, repo){
-        $.ajax({
-            url: "https://api.github.com/repos/" + repo.name,
-            dataType: 'jsonp',
-            success: function(data){
-                count+=1;
-
-                data = data.data;
-                $("#projects").append(
-                    `<div class="col-sm-3 custom-card">
-                            <a href="${data.html_url}"><h5>${data.name}</h5></a>
-                            <div>${generateTagHtml(data.topics)}</div>
-                            <div>${generateDescriptionHtml(data.description)}</div>
-                            <div>${generateLanguageHtml(data.language)}</div>
-                    </div>`
-                );
-            }
-        })
-    });
-}
-
-function getRepos(username){
-    $.ajax({
-        url: "https://api.github.com/users/" + username + "/repos?type=all&sort=pushed",
-        dataType: 'jsonp',
-        success: function(data){
-            $.each(data.data, function(index, repo){
-                if(count < COUNT_MAX && repo.topics.length > 0 && repo.description != null){
-                    count += 1;
-                    
-                    $("#projects").append(
-                        `<div class="col-sm-3 custom-card">
-                            <a href="${repo.html_url}"><h5>${repo.name}</h5></a>
-                            <div>${generateTagHtml(repo.topics)}</div>
-                            <div>${generateDescriptionHtml(repo.description)}</div>
-                            <div>${generateLanguageHtml(repo.language)}</div>
-                        </div>`
-                    );
-                }         
-            });
-
-            if (count >= COUNT_MAX){
-                $("#projects").append(
-                    `<div class="col-sm-3 custom-card d-flex align-items-center justify-content-center">
-                        <a href="https://github.com/${username}?tab=repositories"><h5>See more →</h5></a>
-                    </div>`
-                );
-            }
-        }
-    });
-}
+let countProjects = 0;
+let countWorkshops = 0;
+let countExperiences = 0;
 
 function generateDescriptionHtml(description){
     if (description == null){
@@ -106,14 +54,62 @@ function getLanguageColor(language){
     return result;
 }
 
-function getWorkshops(workshops){
-    $.each(workshops, function(index, workshop){
-            $("#workshops").append(
-                `<div class="col-sm-3 custom-card">
-                    <h5><a href="${workshop.url}">${workshop.title}</a></h5>
-                    <p>${workshop.time}</p>
-                    <p>${workshop.description}</p>
+function getRepos(username){
+    $.ajax({
+        url: "https://api.github.com/users/" + username + "/repos?type=all&sort=pushed",
+        dataType: 'jsonp',
+        success: function(data){
+            $.each(data.data, function(index, repo){
+                if(countProjects <= COUNT_MAX-1 && repo.topics.length > 0 && repo.description != null && !repo.description.includes("practice")){
+                    countProjects++;
+                    
+                    $("#projects").append(
+                        `<div class="col-sm-3 custom-card">
+                            <a href="${repo.html_url}"><h5>${repo.name}</h5></a>
+                            <div>${generateTagHtml(repo.topics)}</div>
+                            <div>${generateDescriptionHtml(repo.description)}</div>
+                            <div>${generateLanguageHtml(repo.language)}</div>
+                        </div>`
+                    );
+                }         
+            });
+
+            $("#projects").append(
+                `<div class="col-sm-3 custom-card d-flex align-items-center justify-content-center">
+                    <a href="https://github.com/${username}?tab=repositories"><h5>See more →</h5></a>
                 </div>`
             );
+        }
+    });
+}
+
+function getWorkshops(workshops){
+    $.each(workshops, function(index, workshop){
+            if (countWorkshops <= COUNT_MAX/2) {
+                countWorkshops++;
+                $("#workshops").append(
+                    `<div class="col-sm-3 custom-card">
+                        <h5>${workshop.title}</h5>
+                        <p>${workshop.time}</p>
+                        <p>${workshop.description}</p>
+                    </div>`
+                );
+            }
+    });
+}
+
+function getExperiences(experiences){
+    $.each(experiences, function(index, experience){
+            if (countExperiences <= COUNT_MAX/2) {
+                countExperiences++;
+                $("#experiences").append(
+                    `<div class="col-sm-3 custom-card">
+                        <h5>${experience.title}</h5>
+                        <h6>${experience.company}</h6>
+                        <p>${experience.time}</p>
+                        <p>${experience.description}</p>
+                    </div>`
+                );
+            }
     });
 }
